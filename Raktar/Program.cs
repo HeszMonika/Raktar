ec1c91d083ac46df12f93fd1e9f1269f9ec3ec12
HeszMonika <heszmoni2001@gmail.com>
2020-09-24 07:29:43 +0100

    BeolvasRaktar() metódus elkészítése.

--- Raktar/Program.cs
+++ Raktar/Program.cs
@@ -3,15 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Raktar
 {
     class Program
     {
+        static List<Termek> termekek = new List<Termek>();
+
+        static void BeolvasRaktar()
+        {
+            StreamReader raktar = new StreamReader("raktar.csv");
+
+            while (!raktar.EndOfStream)
+            {
+                //Sor[0] - Kód
+                //Sor[1] - Név
+                //Sor[2] - Kód
+                //Sor[3] - Db
+                //Console.WriteLine(raktar.ReadLine());//Kiírjuk a tartalmat.
+                string[] sor = raktar.ReadLine().Split(';');
+                termekek.Add(new Termek(sor[0], sor[1], Convert.ToInt32(sor[2]), Convert.ToInt32(sor[3])));
+            }
+            raktar.Close();
+        }
+
         static void Main(string[] args)
         {
             //Termek t = new Termek("P01", "Póló XXL", 1500, 10);
             //Console.WriteLine($"{t.Kod} - {t.Nev} - {t.Ar} - {t.Db}");
+
+            BeolvasRaktar();
 
             Console.ReadKey();
         }
